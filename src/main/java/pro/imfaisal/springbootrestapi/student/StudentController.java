bf55c2a31f06134b6069561d709103674b7391e2
Faisal Hussain <faisal@base29.com>
2021-03-06 14:53:54 +0500

    added route mapping and method for diplaying a list of students

--- src/main/java/pro/imfaisal/springbootrestapi/student/StudentController.java
+++ src/main/java/pro/imfaisal/springbootrestapi/student/StudentController.java
@@ -1,5 +1,18 @@
 package pro.imfaisal.springbootrestapi.student;
 
-public class StudentController {
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(new Student(1L, "Faisal", "faisalblink@gmail.com", LocalDate.of(1981, Month.JUNE, 8), 35));
+    }
 }
