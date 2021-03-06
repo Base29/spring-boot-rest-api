6d2a984bfa62ff56019259d7158229e4ab99ab58
Faisal Hussain <faisal@base29.com>
2021-03-06 14:13:05 +0500

    created Contructors for Student class

--- src/main/java/pro/imfaisal/springbootrestapi/student/Student.java
+++ src/main/java/pro/imfaisal/springbootrestapi/student/Student.java
@@ -1,5 +1,34 @@
 package pro.imfaisal.springbootrestapi.student;
 
-public class Student {
+import java.time.LocalDate;
 
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    public Student() {
+
+    }
+
+    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+
+    }
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
+
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+
+    }
 }
