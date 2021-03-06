1d22fb96d1f0cea36bc11d0df57d7b16dd3b21cf
Faisal Hussain <faisal@base29.com>
2021-03-06 14:00:13 +0500

    created a Student class

--- src/main/java/pro/imfaisal/springbootrestapi/SpringBootRestApiApplication.java
+++ src/main/java/pro/imfaisal/springbootrestapi/SpringBootRestApiApplication.java
@@ -1,5 +1,7 @@
 package pro.imfaisal.springbootrestapi;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +16,8 @@ public class SpringBootRestApiApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World!";
+	public List<String> hello() {
+		return List.of("Hello", "World!");
 	}
 
 }
