d57f3340435acd0924becf1218a8cd941264761e
Faisal Hussain <faisal@base29.com>
2021-03-06 13:46:26 +0500

    started developing application

---
+++ src/test/java/pro/imfaisal/springbootrestapi/SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.imfaisal.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
