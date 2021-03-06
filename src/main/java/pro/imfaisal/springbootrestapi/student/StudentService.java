30df247eadc8e2262af20d012bdf438c2e540395
Faisal Hussain <faisal@base29.com>
2021-03-06 16:17:35 +0500

    Added student repository

--- src/main/java/pro/imfaisal/springbootrestapi/student/StudentService.java
+++ src/main/java/pro/imfaisal/springbootrestapi/student/StudentService.java
@@ -1,14 +1,21 @@
 package pro.imfaisal.springbootrestapi.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(1L, "Faisal", "faisalblink@gmail.com", LocalDate.of(1981, Month.JUNE, 8), 35));
+        return studentRepository.findAll();
     }
 }
