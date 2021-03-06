253b451d29ebd5ba32cfba882deb34b2d47186aa
Faisal Hussain <faisal@base29.com>
2021-03-06 14:13:58 +0500

    added getters and setters

--- src/main/java/pro/imfaisal/springbootrestapi/student/Student.java
+++ src/main/java/pro/imfaisal/springbootrestapi/student/Student.java
@@ -31,4 +31,75 @@ public class Student {
         this.age = age;
 
     }
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return LocalDate return the dob
+     */
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    /**
+     * @param dob the dob to set
+     */
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    /**
+     * @return Integer return the age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
